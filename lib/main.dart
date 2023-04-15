d7c847c9cb5bc3bd561f96bac2cd31fbcd155a61
Shahanaj2000 <shahanajmt@gmail.com>
2023-04-15 23:15:39 +0530

    ChatApp | Firebase Connections Done

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:chatappfirebase/pages/home_page.dart';
+import 'package:chatappfirebase/shared/constants.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    // run the initialization to web
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: Constants.apiKey,
+          appId: Constants.appId,
+          messagingSenderId: Constants.messagingSenderId,
+          projectId: Constants.projectId),
+    );
+  } else {
+    // run the initialization to android and ios
+    await Firebase.initializeApp(); 
+  }
+  
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ChatApp Firebase',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,7 @@
+class Constants {
+  static String apiKey = "AIzaSyDjWF5eTDiGRQyfFsFiR1QIpKiX520OqM0";
+  static String appId = "1:944023556520:web:9070b80511bc46cb608d9d";
+  static String messagingSenderId = "944023556520";
+  static String projectId = "chatapp-firebase-dd4d4";
+  
+}
